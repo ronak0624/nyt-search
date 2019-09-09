f0cbf75c0b160e7bccfbc6da4998c73459c5e7d9
Ronak Patel <ronak0624@gmail.com>
2019-09-09 13:42:21 -0700

    added cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,14 @@ var search, startYear, endYear, numRecords;
 
 var apiKey = "woj581RvIYanR1pRdG32Uod5Vs6AcjyA";
 
+function newArticle(title, summary, pubDate){
+    var article = $("<div>");
+    article.attr("class", "card single-article");
+    article.append("<h5 class='title'>" + title + "</h5>");
+    article.append("<p class='summary'>" + summary + "</p>");
+    article.append("<p class='pub-date'>" + pubDate + "</p>");
+    $(".articles").append(article);
+}
 
 $(".submit-button").click(function () {
     // search = $(".search-input").val();
