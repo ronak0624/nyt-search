521b8a3491a53a27033fc9db79e44f879f8b28eb
Ronak Patel <ronak0624@gmail.com>
2019-09-09 12:54:11 -0700

    js changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,21 +2,25 @@ var search, startYear, endYear, numRecords;
 
 var apiKey = "woj581RvIYanR1pRdG32Uod5Vs6AcjyA";
 
-$(".submit-button").click(function(){
-    search = $(".search-input").val();
-    numRecords = $(".num-records").val();
-    startYear = $(".start-year").val();
-    endYear = $(".end-year").val();
-    var queryURL =  "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + 
-                    ""
-                    "&api-key=" + yourkey;
-                    
-});
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-
+$(".submit-button").click(function () {
+    // search = $(".search-input").val();
+    // numRecords = $(".num-records").val();
+    // startYear = $(".start-year").val();
+    // endYear = $(".end-year").val();
+    search = "obama"
+    numRecords = 0;
+    startYear = "201220101"
+    endYear = "20121231"
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search +
+        "&begin_date=" + startYear +
+        "&end_year=" + endYear +
+        "&api-key=" + yourkey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        
+    });
 
 });
